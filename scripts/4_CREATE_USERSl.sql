e9f43f8745a05c9cc14bddb2aab341e58cbc82ec
Sergio Vásquez <sergiovasquez48575@correo.itm.edu.co>
2019-10-06 08:05:54 -0500

    Update 4_CREATE_USERSl.sql

--- scripts/4_CREATE_USERSl.sql
+++ scripts/4_CREATE_USERSl.sql
@@ -31,40 +31,47 @@ CREATE USER auditor2 IDENTIFIED BY auditor2;
 
 ALTER USER developer 
 PROFILE developer; 
-
+GRANT CREATE SESSION TO developer;
 
 ALTER USER web_application 
 PROFILE web_application;
-
+GRANT CREATE SESSION TO web_application;
 
 ALTER USER dba_admin 
 PROFILE dba_admin;
-
+GRANT CREATE SESSION TO dba_admin;
 
 ALTER USER analyst 
 PROFILE analyst;
-
+GRANT CREATE SESSION TO analyst;
 
 ALTER USER support_III 
 PROFILE support_III;
+GRANT CREATE SESSION TO support_III;
 
 
 ALTER USER reporter 
 PROFILE reporter;
+GRANT CREATE SESSION TO reporter;
 
 ALTER USER auditor 
 PROFILE auditor;
+GRANT CREATE SESSION TO auditor;
+
 
 ALTER USER developer2 
 PROFILE developer;
+GRANT CREATE SESSION TO developer2;
 
 
 ALTER USER analyst2 
 PROFILE analyst;
+GRANT CREATE SESSION TO analyst2;
 
 
 ALTER USER auditor2 
 PROFILE auditor;
+GRANT CREATE SESSION TO auditor2;
 
 
 
