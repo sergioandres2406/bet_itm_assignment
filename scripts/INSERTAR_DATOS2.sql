b0c688deae5c80c4a39da56d1682950d3300f17f
CHRISTIAN ALONSO FONNEGRA YEPES <cristianlk16@gmail.com>
2019-11-17 14:27:24 -0500

    Update INSERTAR_DATOS2.sql
    R.

--- scripts/INSERTAR_DATOS2.sql
+++ scripts/INSERTAR_DATOS2.sql
@@ -1,1 +1,9 @@
 --INSERTAR MAS DATOS
+
+
+--PREFERENCIAS
+INSERT INTO preferencias (id_usuario,nombre,idioma_correo,boletin_informativo,correo_apuesta_deporte,envio_sms,notificaciones_navegador,aviso_boleta_apuesta,registro_activo) VALUES (1,'Zenaida, Jeanette, Arthur, Signe','aleman','bienvenido alas apuestas','odio.semper@elit.net','N','N','N','Y');
+INSERT INTO preferencias (id_usuario,nombre,idioma_correo,boletin_informativo,correo_apuesta_deporte,envio_sms,notificaciones_navegador,aviso_boleta_apuesta,registro_activo) VALUES (2,'Scarlet, Lysandra, Kato, Rebecca','español','bienvenido alas apuestas','netus.et@In.edu','Y','Y','Y','Y');
+INSERT INTO preferencias (id_usuario,nombre,idioma_correo,boletin_informativo,correo_apuesta_deporte,envio_sms,notificaciones_navegador,aviso_boleta_apuesta,registro_activo) VALUES (3,'Iola, Channing, Wyatt, Drake','english','bienvenido alas apuestas','enim@eu.ca','Y','Y','Y','Y');
+INSERT INTO preferencias (id_usuario,nombre,idioma_correo,boletin_informativo,correo_apuesta_deporte,envio_sms,notificaciones_navegador,aviso_boleta_apuesta,registro_activo) VALUES (4,'Gloria, Casey, Carla, Ali','aleman','bienvenido alas apuestas','ipsum.Curabitur.consequat@Vivamussit.com','Y','N','N','Y');
+INSERT INTO preferencias (id_usuario,nombre,idioma_correo,boletin_informativo,correo_apuesta_deporte,envio_sms,notificaciones_navegador,aviso_boleta_apuesta,registro_activo) VALUES (5,'Flynn, Herrod, Ruth, Sacha','english','bienvenido alas apuestas','lobortis.mauris@sociisnatoquepenatibus.edu','N','Y','N','Y');
