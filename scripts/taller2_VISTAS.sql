6eddffc32efa53ce9a6d22d676949a49b0cc6cce
jadisonyulian <jadisonyulian@gmail.com>
2019-11-17 19:51:17 -0500

    Actualizacion Vistas

--- scripts/taller2_VISTAS.sql
+++ scripts/taller2_VISTAS.sql
@@ -23,7 +23,7 @@ order by Nombre
 ;
 
 /*  VISTA 1 */
-CREATE OR REPLACE VIEW GANADORES_SEMANALES 
+CREATE OR REPLACE VIEW V_GANADORES_SEMANALES 
  AS SELECT  u.nombre1 ||' '||u.nombre2||' '||u.apellido1||' '||u.apellido2 as Nombre,Sum( NVL(a.total_ganado,0)) VALOR_GANADO FROM USUARIOS U INNER JOIN APUESTAS A ON U.ID= a.id_usuario
 WHERE a.fecha_apuesta between trunc(sysdate, 'DAY') and trunc(sysdate, 'DAY')+6
 group by u.nombre1 ||' '||u.nombre2||' '||u.apellido1||' '||u.apellido2
@@ -44,7 +44,7 @@ La idea es que cuando se llame la vista se pueda pasar el id de la apuesta para 
 ejemplo: SELECT * FROM DETALLES_APUESTAS WHERE APUESTAS.ID = 123
 */
 
-CREATE OR REPLACE VIEW DETALLES_APUESTAS
+CREATE OR REPLACE VIEW V_DETALLES_APUESTAS
 AS
 SELECT da.id_apuesta ID,cp.estado Estado_Partido,TO_DATE(TO_CHAR(cp.fecha, 'MM/DD/YYYY'), 'MM/DD/YYYY') Fecha_Partido, TO_CHAR(cp.fecha,'HH:MI:SS') HORA,
 e1.nombre ||' - '||e2.nombre PARTIDO, ta.tipo_apuesta TIPO_APUESTA,
@@ -60,7 +60,7 @@ CASE WHEN da.valor_ganado=0 then 'PERDIO' WHEN da.valor_ganado is null then 'PEN
 FROM detalle_apuesta da inner join tipos_apuestas ta on 
 da.id_tipo_apuesta=ta.id inner join cronograma_partidos cp on ta.id_cronograma=cp.id
 inner join equipos e1 on cp.id_equipo1=e1.id
-inner join equipos e2 on cp.id_equipo2=e2.id
+inner join equipos e2 on cp.id_equipo2=e2.id;
 
 
 
@@ -71,15 +71,20 @@ corresponderá a cada columna (Omitir la siguiente columna Pago máx. incl. 5% bon
 muestre la información únicamente de esa apuesta en particular: SELECT * FROM RESUMEN_APUESTAS WHERE APUESTAS.ID = 123.
 */ 
 
-CREATE OR REPLACE VIEW RESUMEN_APUESTAS
+CREATE OR REPLACE VIEW V_RESUMEN_APUESTAS
 AS
-SELECT ID_APUESTA ID ,COUNT (*) NUMERO_APUESTAS, SUM(valor_apostado) TOTAL_VALOR APOSTADO,
+SELECT ID_APUESTA ID ,COUNT (*) NUMERO_APUESTAS, SUM(valor_apostado) TOTAL_VALOR_APOSTADO, 
 MAX(CASE WHEN da.porcentaje_equipo1_apostado is NOT null then da.porcentaje_equipo1_apostado
 WHEN da.porcentaje_equipo2_apostado is NOT null then da.porcentaje_equipo2_apostado
 WHEN da.porcentaje_empate_apostado is NOT null then da.porcentaje_empate_apostado
 WHEN da.porcentaje_si_apostado  is NOT null then da.porcentaje_si_apostado
 WHEN da.porcentaje_no_apostado  is NOT null then da.porcentaje_no_apostado
 WHEN da.porcentaje_mas_apostado  is not null then da.porcentaje_mas_apostado
-WHEN da.porcentaje_menos_apostado  is not null then da.porcentaje_menos_apostado ELSE  0 end) CUOTA_MAXIMA
-FROM  detalle_apuestas da
-WHERE 
+WHEN da.porcentaje_menos_apostado  is not null then da.porcentaje_menos_apostado ELSE  0 end) CUOTA_MAXIMA,
+SUM(da.valor_ganado) TOTAL_VALOR_GANADO
+FROM  DETALLE_APUESTA da
+group by ID_APUESTA;
+
+
+
+
